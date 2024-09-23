1586dda93ef38ba4bef1a0891b36a4c9ff25d85d
moyuren_home <moyuren>
2024-09-23 20:11:23 +0800

    [新增]

--- src/views/article/components/ArticleEdit.vue
+++ src/views/article/components/ArticleEdit.vue
@@ -1,11 +1,34 @@
 <script setup>
 import { ref } from 'vue'
+import ChannelSelect from './ChannelSelect.vue'
 // 控制抽屉显示隐藏
 const visibleDrawer = ref(false)
 
+// 默认空白数据
+const defaultForm = {
+  title: '', // 标题
+  cate_id: '', // 分类id
+  content: '', // 内容
+  cover_img: '', // 封面
+  state: '' // 状态
+}
+
+// 准备数据
+const formModel = ref({
+  ...defaultForm
+})
+
 const open = (row) => {
   visibleDrawer.value = true // 显示抽屉
-  console.log(row)
+
+  if (row.id) {
+    console.log('编辑回显')
+  } else {
+    formModel.value = {
+      ...defaultForm
+    }
+    console.log('添加')
+  }
 }
 
 defineExpose({
@@ -14,7 +37,31 @@ defineExpose({
 </script>
 
 <template>
-  <el-drawer v-model="visibleDrawer" title="大标题" size="50%" direction="rtl">
-    <span>Hi</span>
+  <el-drawer
+    v-model="visibleDrawer"
+    :title="formModel.id ? '编辑文章' : '添加文章'"
+    direction="rtl"
+    size="50%"
+  >
+    <!-- 发表文章表单 -->
+    <el-form :model="formModel" ref="formRef" label-width="100px">
+      <el-form-item label="文章标题" prop="title">
+        <el-input v-model="formModel.title" placeholder="请输入标题"></el-input>
+      </el-form-item>
+      <el-form-item label="文章分类" prop="cate_id">
+        <channel-select
+          v-model="formModel.cate_id"
+          width="100%"
+        ></channel-select>
+      </el-form-item>
+      <el-form-item label="文章封面" prop="cover_img"> 文件上传 </el-form-item>
+      <el-form-item label="文章内容" prop="content">
+        <div class="editor">富文本编辑器</div>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary">发布</el-button>
+        <el-button type="info">草稿</el-button>
+      </el-form-item>
+    </el-form>
   </el-drawer>
 </template>
