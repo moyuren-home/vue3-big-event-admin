faa8db0a4171400825f5f674ad2bf0d4f3254433
moyuren <2092664654@qq.com>
2024-10-02 00:53:41 +0800

    [新增]个人基本资料框架搭建

--- src/views/user/UserProfile.vue
+++ src/views/user/UserProfile.vue
@@ -1,1 +1,9 @@
-<template>个人详情</template>
+<script setup>
+import PageContainer from '@/components/PageContainer.vue'
+</script>
+
+<template>
+  <page-container title="基本资料">
+    <!-- 表单部分 -->
+  </page-container>
+</template>
