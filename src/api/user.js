b63f900beae2513edb153593b7de12cdb5501e2d
moyuren <2092664654@qq.com>
2024-10-17 14:17:03 +0800

    [新增]用户密码修改及用户头像上传

--- src/api/user.js
+++ src/api/user.js
@@ -14,3 +14,11 @@ export const userGetInfoService = () => request.get('/my/userinfo')
 // 更新用户基本信息
 export const userUpdateInfoService = ({ id, nickname, email }) =>
   request.put('/my/userinfo', { id, nickname, email })
+
+// 更新用户头像
+export const userUploadAvatarService = (avatar) =>
+  request.patch('/my/update/avatar', { avatar })
+
+// 更新用户密码
+export const userUpdatePassService = ({ old_pwd, new_pwd, re_pwd }) =>
+  request.patch('/my/updatepwd', { old_pwd, new_pwd, re_pwd })
